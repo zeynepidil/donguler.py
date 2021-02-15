e4b1b95fc28b5bc90f1f94bca12880026e338ff0
Zeynep İdil Erkal <zeynepidilerkal99@gmail.com>
2021-02-15 16:51:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def ödevleriListele():
+ ödevler=["ödev1","ödev2"]
+ for ödev in ödevler:
+  print(ödevler)
+
+ödevleriListele()
---
+++ sartblogu.py
@@ -0,0 +1,9 @@
+x=ödev1
+y=ödev2
+
+if x>y
+ print('ödev azalışı')
+ elif y>x
+ print('ödev artışı')
+ else
+ print('ödev yok')
